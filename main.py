b1a4fece04a8306363e8c47ecb8553249bbb4020
Gabe Dumbrille <gndumbri@gmail.com>
2020-04-09 10:03:33 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, request, redirect
+
+import webbrowser
+import pandas as pd
+import numpy as np
+
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import load_model
+import os
+
+app = Flask(__name__)
+
+app.config["IMAGE_UPLOADS"]= "uploads/unknown_photo"
+#/Users/gabrieldumbrille/Desktop/COPYFMP/
+@app.route("/", methods=["GET", "POST"])
+
+def upload_img():
+
+    if request.method == "POST":
+
+        if request.files:
+
+            image = request.files["image"]
+
+            image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
+
+            print("Image saved!")
+
+            return redirect(request.url)
+
+    return render_template("upload_img.html", methods=["GET", "POST"])
+
+@app.route("/find_my_photo")
+
+def find_my_photo():
+    photo = 'uploads'
+    #/Users/gabrieldumbrille/Desktop/
+    locations = pd.read_csv(r'key.csv')
+    #/Users/gabrieldumbrille/Desktop/
+    model = load_model(r'MobileNet_Model.h5')
+    #/Users/gabrieldumbrille/Desktop/
+
+    pred = ImageDataGenerator(rescale=1./255)
+
+    pred = pred.flow_from_directory(
+        photo,
+        target_size=(160, 160),
+        batch_size=1,
+        shuffle=False
+        )
+
+    predictions = model.predict(pred, steps=1)
+
+    preds = np.argmax(predictions,axis=1)
+
+    answer = locations.loc[locations['name'].index == preds[0], 'coord']
+
+    street_view =  (''.join(list(answer))).replace(' ', '')
+
+    webbrowser.open_new_tab(
+        'http://maps.google.com/maps?q=&layer=c&cbll=' + street_view)
+
+    return 'Found!'
+
+if __name__ == "__main__":
+    app.run(debug=True)
